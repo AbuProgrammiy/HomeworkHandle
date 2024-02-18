31700221083ce9323711b2bc94bd01c11ef5f622
Abu_Programmiy <abdukholiq23@gmail.com>
2024-02-18 21:21:51 +0500

    REPO tugadi

--- RepoHome/Homework2/Controllers/StudentController.cs
+++ RepoHome/Homework2/Controllers/StudentController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Homework2.Entities.DTOs;
+using Homework2.Models;
+using Homework2.RepoManagment.IRepository;
+using Homework2.RepoManagment.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Homework2.Controllers
 {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        public readonly IStudentsRepository _studentsRepositories;
+        public StudentController(IStudentsRepository repos)
         {
-            return View();
+            _studentsRepositories = repos;
+        }
+
+        [HttpPost]
+        public string Create(StudentDTO student)
+        {
+            return _studentsRepositories.CreateStudent(student);
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {
+            return _studentsRepositories.GetAllStudents();
+        }
+
+        [HttpPut]
+        public string Update(int id, StudentDTO student)
+        {
+            return _studentsRepositories.UpdateStudent(id, student);
+        }
+
+        [HttpDelete]
+        public string Delete(int id)
+        {
+            return _studentsRepositories.DeleteStudent(id);
         }
     }
 }
