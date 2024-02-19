ad0f5c219b4768f44dc8f921c3cb66f46c7b7a92
Abu_Programmiy <abdukholiq23@gmail.com>
2024-02-19 08:10:20 +0500

    Va nihoyat tugadi

--- RepoHome/Homework2/Models/StudentsWithDishes.cs
+++ RepoHome/Homework2/Models/StudentsWithDishes.cs
@@ -1,10 +1,12 @@
-﻿namespace Homework2.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homework2.Models
 {
     public class StudentsWithDishes
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string LunchDish { get; set; }
+        public string LaunchDish { get; set; }
         public string DinnerDish { get; set; }
     }
 }
--- RepoHome/Homework2/Services/ShowStudentsDishService/ShowAllStudentsDishService.cs
+++ RepoHome/Homework2/Services/ShowStudentsDishService/ShowAllStudentsDishService.cs
@@ -6,14 +6,15 @@ namespace Homework2.Services.ShowStudentsDish
 {
     public class ShowAllStudentsDishService : IShowAllStudentsDishService
     {
-        public NpgsqlConnection Conncetion;
-        public string connectionString = "Server=localhost;Port=5432;Database=Homework;User Id=postgres;Password=root;";
+        public NpgsqlConnection Conncetion=new NpgsqlConnection(connectionString);
+        public static string connectionString = "Server=localhost;Port=5432;Database=Homework;User Id=postgres;Password=root;";
+
 
         public IEnumerable<StudentsWithDishes> ShowAllStudentsWithDishes()
         {
-            return Conncetion.Query<StudentsWithDishes>("select s.id, s.name, l.dishname,d.dishname from students as s " +
-                "inner join buffet as l on l.id=student.launchId " +
-                "inner join buffet as d on d.id=student.dinnerId;").ToList();
+            return Conncetion.Query<StudentsWithDishes>("select s.id, s.name, l.dishname as launchDish, d.dishname as dinnerDish from students as s " +
+                "inner join buffet as l on l.id=s.launchId " +
+                "inner join buffet as d on d.id=s.dinnerId;");
         }
     }
 }
