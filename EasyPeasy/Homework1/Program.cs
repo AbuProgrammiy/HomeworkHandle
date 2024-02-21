51449755aa2af9baef047c8f08b8ad5b8deae599
Abu_Programmiy <abdukholiq23@gmail.com>
2024-02-21 09:36:15 +0500

    uy ishi tugadi

---
+++ EasyPeasy/Homework1/Program.cs
@@ -0,0 +1,77 @@
+﻿using Homework1;
+using System.Text.Json;
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Buxalter> buxalters = new List<Buxalter>()
+            {
+                new Buxalter
+                {
+                    Id = 1,
+                    Name = "Abduxoliq",
+                    ProgrammingLanguageId = 1,
+                },
+                new Buxalter
+                {
+                    Id = 2,
+                    Name = "Abdulloh",
+                    ProgrammingLanguageId = 0,
+                },
+                new Buxalter
+                {
+                    Id = 3,
+                    Name = "Otabek",
+                    ProgrammingLanguageId = 2,
+                },
+                new Buxalter
+                {
+                    Id = 4,
+                    Name = "Izzat",
+                    ProgrammingLanguageId = 0,
+                },
+            };
+
+            IEnumerable<ProgrammingLanguage> programmingLanguages = new List<ProgrammingLanguage>()
+            {
+                new ProgrammingLanguage
+                {
+                    Id= 1,
+                    Name= "C#"
+                },
+                new ProgrammingLanguage
+                {
+                    Id= 2,
+                    Name= "Python"
+                },
+                new ProgrammingLanguage
+                {
+                    Id= 3,
+                    Name= "Rust"
+                },
+                new ProgrammingLanguage
+                {
+                    Id= 4,
+                    Name= "C"
+                },
+            };
+
+            //Programmingni biladigan dasturchilarni olish
+            IEnumerable<Buxalter> ProgrammerBuxalters = buxalters.Where(b=>b.ProgrammingLanguageId!=0);
+            ProgrammerBuxalters = buxalters.Join(programmingLanguages,
+                buxalter => buxalter.ProgrammingLanguageId,
+                programmingLanguage => programmingLanguage.Id,
+                (buxalter, programmingLanguage) => buxalter);
+
+            string data =JsonSerializer.Serialize(ProgrammerBuxalters,new JsonSerializerOptions() { WriteIndented=true});
+            Console.WriteLine(data+"\n"+new string('-',50));
+            
+            // C# biladigan buxaltrlar
+            IEnumerable<Buxalter> DotNetProgrammers = buxalters.Where(b => b.ProgrammingLanguageId == 1);
+            data=JsonSerializer.Serialize(DotNetProgrammers,new JsonSerializerOptions() { WriteIndented=true});
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ EasyPeasy/Homework1/ProgrammingLanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class ProgrammingLanguage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
